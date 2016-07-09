b59e9edcbc80f4a3d291afdaf77c22e15ae3077a
Adam Kelly <adam@adamkellydesign.com>
2016-07-10 00:10:03 +0100

    Add Tests for Encode
    Add New Tests,
    Push Forward Version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,8 @@
 __Initial release__
 New Features:
 * Decode Morse Code
+
+### [0.2.0](https://github.com/adamisntdead/marconi/releases/tag/v0.2.0)
+__Initial release__
+New Features:
+* Encode to Morse Code
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,18 @@
 import { expect } from 'chai';
 import { decode } from '../src';
+import { encode } from '../src';
 
 describe('decode', () => {
-  it('should translate "- . ... -   -.-. --- -.. ." to "TEST CODE"', () => {
+  it('should translate a string of morse code, into text', () => {
     expect(decode('- . ... -   -.-. --- -.. .')).to.equal('TEST CODE');
+    expect(decode(' ... ---  ')).to.equal('SO');
+    expect(decode('.---- ----- -----')).to.equal('100');
+  });
+});
+
+describe('encode', () => {
+  it('should translate a string of test, into morse code', () => {
+    expect(encode('Banter')).to.equal('-... .- -. - . .-.');
+    expect(encode('myName is, jeff100')).to.equal('-- -.-- -. .- -- .   .. ... --..--   .--- . ..-. ..-. .---- ----- -----');
   });
 });
